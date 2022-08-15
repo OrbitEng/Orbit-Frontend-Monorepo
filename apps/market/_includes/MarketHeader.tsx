c924c138043fc991e8e889d23303013352b57906
0xMillionz <madokakaname99@protonmail.com>
2022-08-14 22:41:49 -0400

    wallet adapters

--- apps/market/_includes/MarketHeader.tsx
+++ apps/market/_includes/MarketHeader.tsx
@@ -6,7 +6,7 @@ import OrbitLogo from '../public/OrbitLogo.png'
 export const HomeHeader: FC = ({}) => {
 	return(
 		<header className="top-0 w-full h-14 sticky flex">
-			<div className="bg-gradient-to-r from-homeheadergray to-homeheaderpurple w-full h-full flex flex-row justify-evenly">
+			<div className="bg-gradient-to-r from-homeheadergray to-homeheaderpurple w-full h-full flex flex-row justify-evenly border-[#1A1A23] border-b-2">
 				<div className="relative basis-1/4 items-center">
 					<Image
 						src={OrbitLogo}
@@ -18,7 +18,7 @@ export const HomeHeader: FC = ({}) => {
 					searchbar
 				</div>
 				<div className="basis-1/4">
-					options
+					Features	
 				</div>
 			</div>
 		</header>
