de5dc98b75d1e67858018f70453a7ee769df5a24
0xMillionz <madokakaname99@protonmail.com>
2022-08-15 23:15:46 -0400

    responsive header

--- apps/market/_includes/MarketHeader.tsx
+++ apps/market/_includes/MarketHeader.tsx
@@ -8,18 +8,20 @@ import OrbitLogo from '../public/OrbitLogo.png'
 export const HomeHeader: FC = ({}) => {
 	return(
 		<header className="top-0 w-full h-14 sticky flex">
-			<div className="bg-gradient-to-r from-homeheadergray to-homeheaderpurple w-full h-full flex flex-row justify-evenly border-[#1A1A23] border-b-2 align-middle">
-				<div className="relative basis-1/4 py-auto align-middle my-auto ml-2">
+			<div className="bg-gradient-to-r from-homeheadergray to-homeheaderpurple w-full h-full flex flex-row justify-around border-[#1A1A23] border-b-2 align-middle">
+				<div className="relative basis-1/4 py-auto align-middle my-2 ml-2">
 					<Image
 						src={OrbitLogo}
-						layout="responsive"
+						width={22}
+						layout="fill"
 						alt="The Name and Logo for the Orbit market"
+						objectFit="contain"
 					/>
 				</div>
 				<div className="basis-1/2">
 					searchbar
 				</div>
-				<div className="flex basis-1/4 align-middle cen">
+				<div className="flex basis-1/4 align-middle content-center">
 					<WalletInteractionsButton />
 				</div>
 			</div>
