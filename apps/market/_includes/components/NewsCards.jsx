8c4433da5e37845a779cdff2338a50e36a9e3c67
0xMillionz <madokakaname99@protonmail.com>
2022-09-01 23:07:11 -0400

    awesome new ish header

--- apps/market/_includes/components/NewsCards.jsx
+++ apps/market/_includes/components/NewsCards.jsx
@@ -10,7 +10,7 @@ export function BetaCard(props) {
 			<div className="flex w-full px-10 pt-5 text-[#4A4A4A] font-bold text-xl">BETA</div>
 			<div className="flex flex-row justify-start w-5/6 mt-6 mb-12">
 				<div className="flex flex-col justify-start align-middle">
-					<p className="text-[#4A4A4A] font-semibold mt-2 mb-2">ENDS 2022 Q4</p>
+					<p className="text-[#4A4A4A] text-lg font-bold mt-2 mb-2">ENDS 2022 Q4</p>
 					<div className="font-bold text-white text-3xl">Beta is now available for all users!</div>
 					<p className="font-bold text-[#4A4A4A]">Thank you for using orbit, beta is only available for a short period.</p>
 					<button className="flex font-bold text-white bg-[#474747] bg-opacity-50 py-2 px-4 rounded-lg mr-auto mt-4">
--- apps/market/_layouts/MarketHome.jsx
+++ apps/market/_layouts/MarketHome.jsx
@@ -5,14 +5,14 @@ import Head from 'next/head'
 
 export function Home(props) {
 	return(
-		<div className="bg-[#070513] w-full min-h-screen">
+		<div className="w-full min-h-screen bg-transparent">
 			<Head>
 				<title>Market Home</title>
 				<link rel="icon" href="favicon.ico" />
 			</Head>
-			<main className="">
+			<main className="bg-[url('/bgWallpaper.png')] bg-cover">
 				<HomeHeader/>
-				<div className="max-w-7xl align-center mx-auto">
+				<div className="max-w-5xl align-center mx-auto">
 					<HomeBanner />
 					<HomeProductExplorer />
 				</div>
