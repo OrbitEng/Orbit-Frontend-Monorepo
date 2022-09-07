daf8f53a99f4af2ca393038c0b46bbaa75a9c4b4
0xMillionz <madokakaname99@protonmail.com>
2022-09-07 10:03:47 -0400

    forgot to add last night

--- apps/market/pages/product/[productType]/[productId].jsx
+++ apps/market/pages/product/[productType]/[productId].jsx
@@ -5,15 +5,26 @@ import {
 	PhysicalProductLayout,
 } from "@layouts/ProductDisplays";
 
+//dummy products
+const dummyPhys
+
+const dummyService
+
+const dummyDigital = {
+
+}
+
 export default function ProductsPage(props) {
 	const router = useRouter();
 	const { productType, productId } = router.query;
+
+	// fetch product somewhere in here from query
 
 	return (
 		<>
 			{ !productType === "physical" || <PhysicalProductLayout id={productId} /> }
 			{ !productType === "digital" || <DigitalProductLayout id={productId} /> }
-			{ !productType === "service" || <DigitalServiceLayout id={productId} /> }
+			{ !productType === "service" || <DigitalServiceLayout product={} /> }
 		</>
 	)
 }
