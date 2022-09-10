c035e9910d79d6270d84852364cfd287bae148c8
Your Name <you@example.com>
2022-09-10 00:06:01 -0400

    ?

--- apps/market/tailwind.config.js
+++ apps/market/tailwind.config.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './_includes/**/*.{js,ts,jsx,tsx}',
-    './_layouts/**/*.{js,ts,jsx,tsx}'
-  ],
-  theme: {
-    extend: {
-      colors: {
-        homeheaderpurple: 'rgba(49,48,113,0.22)',
-        homeheadergray: 'rgba(26,25,25,0.22)',
-        buttontransparent: 'rgba(24,20,36,0.82)',
-        buttontransparent2: 'rgba(45,42,53,0.95)',
-        searchbartransparent: 'rgba(10,10,10,0.66)',
-        searchbuttontrans: 'rgba(45,42,53,0.7)',
-        searchbuttontrans2: 'rgba(45,42,53,0.1)'
-      }
-    },
-  },
-  plugins: [],
-}
-=======
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -47,4 +22,3 @@ module.exports = {
   },
   plugins: [],
 }
->>>>>>> master
