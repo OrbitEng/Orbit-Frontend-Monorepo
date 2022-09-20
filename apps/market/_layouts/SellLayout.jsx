95e0406eed096a309d2f206342f3dabf6b4e5feb
0xMillionz <madokakaname99@protonmail.com>
2022-09-20 12:38:01 -0400

    starting upload

--- apps/market/_layouts/SellLayout.jsx
+++ apps/market/_layouts/SellLayout.jsx
@@ -18,79 +18,116 @@ export function SellLayout(props){
 			</Head>
 			<main className="bg-[url('/oldbgWallpaper.png')]">
 				<HomeHeader headerMiddle={searchBar}/>
-				<div className="-mt-14 sm:-mt-32 max-w-7xl align-center mx-auto h-view">
+				<div className={(selectedCategory == null ? "-mt-14 sm:-mt-32" : "") + " max-w-7xl align-center mx-auto h-view"}>
 					{
 						(selectedCategory == null || selectedCategory == undefined) ? 
 						(
-							<div className="flex flex-row justify-around w-full mx-auto h-[100vh] gap-24 content-center my-auto">
-								<div className="flex group relative rounded-2xl my-auto h-1/2 w-1/3 hover:scale-[103%] transition duration-700">
-									<div className="bg-[#26308F] absolute -inset-0 bg-opacity-70 rounded-lg blur-xl group-hover:bg-opacity-100 transition duration-700" />
-									<div className="flex flex-col py-4 px-8 relative bg-gradient-to-tr from-[#2c2c2cc0] to-[#4a4a4ac0] w-full h-full rounded-2xl">
-										<div className="relative flex w-1/2 h-1/2 mx-auto">
-											<Image
-												src="/emojis/globeEmojiImage.png"
-												layout="fill"
-												objectFit="contain"
-												width={50}
-											/>
-										</div>
-										<h1 className="text-3xl font-bold text-white mx-auto text-center">Physical</h1>
-										<p className="text-xl text-[#6A6A6A] mx-auto justify-center mt-4 leading-tight text-center">
-											Sell shoes, clothes tech, and much more with orbit, it's just a few clicks away!
-										</p>
-										<div className="rounded-full p-2 my-auto h-14 w-14 bg-gradient-to-tr from-[#0E0C15] to-[#18171D] via-[#161320] mx-auto content-center align-middle">
-											<ArrowRightIcon className="h-10 w-10 text-[#3F46FF] m-auto stroke-2" />
-										</div>
-									</div>
-								</div>
-								<div className="flex group relative rounded-2xl my-auto h-1/2 w-1/3 hover:scale-[103%] transition duration-700">
-									<div className="bg-[#4E268F] absolute -inset-0 bg-opacity-70 rounded-lg blur-xl group-hover:bg-opacity-100 transition duration-700" />
-									<div className="flex flex-col py-4 px-8 relative bg-gradient-to-tr from-[#2c2c2cc0] to-[#4a4a4ac0] w-full h-full rounded-2xl">
-										<div className="relative flex w-1/2 h-1/2 mx-auto">
-											<Image
-												src="/emojis/filesEmojiImage.png"
-												layout="fill"
-												objectFit="contain"
-												width={50}
-											/>
-										</div>
-										<h1 className="text-3xl font-bold text-white mx-auto text-center">Services</h1>
-										<p className="text-xl text-[#6A6A6A] mx-auto justify-center mt-4 leading-tight text-center">
-											Freelance and take comissions through Orbit, start your custom content adventures!
-										</p>
-										<div className="rounded-full p-2 my-auto h-14 w-14 bg-gradient-to-tr from-[#0E0C15] to-[#18171D] via-[#161320] mx-auto content-center align-middle">
-											<ArrowRightIcon className="h-10 w-10 text-[#4E268F] m-auto stroke-2" />
-										</div>
-									</div>
-								</div>
-								<div className="flex group relative rounded-2xl my-auto h-1/2 w-1/3 hover:scale-[103%] transition duration-700">
-									<div className="bg-[#81268F] absolute -inset-0 bg-opacity-70 rounded-lg blur-xl group-hover:bg-opacity-100 duration-700 transition" />
-									<div className="flex flex-col py-4 px-8 relative bg-gradient-to-tr from-[#2c2c2cc0] to-[#4a4a4ac0] w-full h-full rounded-2xl">
-										<div className="relative flex w-1/2 h-1/2 mx-auto">
-											<Image
-												src="/emojis/wrenchEmojiImage.png"
-												layout="fill"
-												objectFit="contain"
-												width={50}
-											/>
-										</div>
-										<h1 className="text-3xl font-bold text-white mx-auto text-center">Digital</h1>
-										<p className="text-xl text-[#6A6A6A] mx-auto justify-center mt-4 leading-tight text-center">
-											Get paid for your digital art, premade designs, beatpacks, private content and more!
-										</p>
-										<div className="rounded-full p-2 my-auto h-14 w-14 bg-gradient-to-tr from-[#0E0C15] to-[#18171D] via-[#161320] mx-auto content-center align-middle">
-											<ArrowRightIcon className="h-10 w-10 text-[#FB3FFF] m-auto stroke-2" />
-										</div>
-									</div>
-								</div>
-							</div>
+							<CategorySelect cat={{selectedCategory, setSelectedCategory}} />	
 						) : (
-							<div></div>
+							<DigitalUpload />
 						)
 					}
 					<MainFooter />
 				</div>
 			</main>
+		</div>
+	)
+}
+
+function DigitalUpload(props) {
+	return(
+		<div className="flex flex-col w-full mx-auto my-auto content-center">
+			<h1 className="text-white font-bold text-4xl my-10">Create New Digital Product</h1>
+			<div className="flex flex-row">
+				<div className="w-1/2">
+					<h3 className="font-bold text-white text-xl">Upload Preview</h3>
+					<span className="text-[#767676]">Formats: jpg, mp4, png</span>
+					<div className="flex border-4 border-dashed border-[#3D3D3D] rounded-2xl w-full h-full content-center align-middle">
+						<span className="align-middle text-center my-auto mx-auto text-2xl font-bold text-white">Drag & Drop Files</span>	
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+function PhysicalUpload(props) {}
+
+function ServiceUpload(props) {}
+
+function CategorySelect(props) {
+	const {selectedCategory, setSelectedCategory} = props.cat;
+	return(
+		<div className="flex flex-row justify-around w-full mx-auto h-[100vh] gap-24 content-center my-auto">
+			<div
+				className="flex group relative rounded-2xl my-auto h-1/2 w-1/3 hover:scale-[103%] transition duration-700"
+				onClick={() => setSelectedCategory("physical")}
+			>
+				<div className="bg-[#26308F] absolute -inset-0 bg-opacity-70 rounded-lg blur-xl group-hover:bg-opacity-100 transition duration-700" />
+				<div className="flex flex-col py-4 px-8 relative bg-gradient-to-tr from-[#2c2c2cc0] to-[#4a4a4ac0] w-full h-full rounded-2xl">
+					<div className="relative flex w-1/2 h-1/2 mx-auto">
+						<Image
+							src="/emojis/globeEmojiImage.png"
+							layout="fill"
+							objectFit="contain"
+							width={50}
+						/>
+					</div>
+					<h1 className="text-3xl font-bold text-white mx-auto text-center">Physical</h1>
+					<p className="text-xl text-[#6A6A6A] mx-auto justify-center mt-4 leading-tight text-center">
+						Sell shoes, clothes tech, and much more with orbit, it's just a few clicks away!
+					</p>
+					<div className="rounded-full p-2 my-auto h-14 w-14 bg-gradient-to-tr from-[#0E0C15] to-[#18171D] via-[#161320] mx-auto content-center align-middle">
+						<ArrowRightIcon className="h-10 w-10 text-[#3F46FF] m-auto stroke-2" />
+					</div>
+				</div>
+			</div>
+			<div
+				className="flex group relative rounded-2xl my-auto h-1/2 w-1/3 hover:scale-[103%] transition duration-700"
+				onClick={() => setSelectedCategory("service")}
+			>
+				<div className="bg-[#4E268F] absolute -inset-0 bg-opacity-70 rounded-lg blur-xl group-hover:bg-opacity-100 transition duration-700" />
+				<div className="flex flex-col py-4 px-8 relative bg-gradient-to-tr from-[#2c2c2cc0] to-[#4a4a4ac0] w-full h-full rounded-2xl">
+					<div className="relative flex w-1/2 h-1/2 mx-auto">
+						<Image
+							src="/emojis/filesEmojiImage.png"
+							layout="fill"
+							objectFit="contain"
+							width={50}
+						/>
+					</div>
+					<h1 className="text-3xl font-bold text-white mx-auto text-center">Services</h1>
+					<p className="text-xl text-[#6A6A6A] mx-auto justify-center mt-4 leading-tight text-center">
+						Freelance and take comissions through Orbit, start your custom content adventures!
+					</p>
+					<div className="rounded-full p-2 my-auto h-14 w-14 bg-gradient-to-tr from-[#0E0C15] to-[#18171D] via-[#161320] mx-auto content-center align-middle">
+						<ArrowRightIcon className="h-10 w-10 text-[#4E268F] m-auto stroke-2" />
+					</div>
+				</div>
+			</div>
+			<div
+				className="flex group relative rounded-2xl my-auto h-1/2 w-1/3 hover:scale-[103%] transition duration-700"
+				onClick={() => setSelectedCategory("digital")}
+			>
+				<div className="bg-[#81268F] absolute -inset-0 bg-opacity-70 rounded-lg blur-xl group-hover:bg-opacity-100 duration-700 transition" />
+				<div className="flex flex-col py-4 px-8 relative bg-gradient-to-tr from-[#2c2c2cc0] to-[#4a4a4ac0] w-full h-full rounded-2xl">
+					<div className="relative flex w-1/2 h-1/2 mx-auto">
+						<Image
+							src="/emojis/wrenchEmojiImage.png"
+							layout="fill"
+							objectFit="contain"
+							width={50}
+						/>
+					</div>
+					<h1 className="text-3xl font-bold text-white mx-auto text-center">Digital</h1>
+					<p className="text-xl text-[#6A6A6A] mx-auto justify-center mt-4 leading-tight text-center">
+						Get paid for your digital art, premade designs, beatpacks, private content and more!
+					</p>
+					<div className="rounded-full p-2 my-auto h-14 w-14 bg-gradient-to-tr from-[#0E0C15] to-[#18171D] via-[#161320] mx-auto content-center align-middle">
+						<ArrowRightIcon className="h-10 w-10 text-[#FB3FFF] m-auto stroke-2" />
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
