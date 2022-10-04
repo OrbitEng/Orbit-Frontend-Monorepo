63960ae85f7d4fd21b0bae39ff37bfb70d7ffaeb
0xMillionz <madokakaname99@protonmail.com>
2022-10-04 13:52:13 -0400

    adding basic account creation modal

--- apps/market/_includes/MarketHeader.jsx
+++ apps/market/_includes/MarketHeader.jsx
@@ -25,6 +25,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 
+import MarketAccountButton from '@includes/components/MarketAccountButton';
 
 export function HomeHeader(props) {
 	const router = useRouter();
@@ -98,8 +99,8 @@ export function HomeHeader(props) {
 				<div className="flex flex-row px-2 gap-3">
 					<div className="bg-gradient-to-tr from-[#181424] via-buttontransparent2 to-buttontransparent border-t-[0.5px] border-[#474747] rounded-full transition hover:scale-[102%]">
 						{
-							wallet ? ( 
-								<WalletConnectButton />
+							!wallet.connected ? ( 
+								<WalletMultiButton />
 							) : (
 								<MarketAccountButton />
 							)
---
+++ apps/market/_includes/components/MarketAccountButton.jsx
@@ -0,0 +1,94 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState, useContext } from 'react'
+
+import MarketAccountsCtx from '@contexts/MarketAccountsCtx';
+import { MarketAccountFunctionalities } from '@functionalities/Accounts';
+
+export default function MarketAccountButton(props) {
+	let [isOpen, setIsOpen] = useState(false);
+	const [nickName, setNickName] = useState();
+	const {marketAccountsClient, setMarketAccountsClient} = useContext(MarketAccountsCtx);
+	const {CreateAccount} = MarketAccountFunctionalities();
+
+	function closeModal() {
+		setIsOpen(false)
+		try{
+			CreateAccount({nickname: nickName}, undefined, undefined);
+		} catch(e) {
+			console.log("someone messed up market account creation with: " + e.message)
+		}		
+	}
+
+	function openModal() {
+		setIsOpen(true)
+	}
+
+  return (
+    <>
+		<button
+			type="button"
+			onClick={openModal}
+			className="px-4 py-2 text-sm font-medium text-white focus:outline-none"
+		>
+			Create Account
+		</button>
+
+		<Transition appear show={isOpen} as={Fragment}>
+			<Dialog as="div" className="relative z-[100]" onClose={closeModal}>
+			<Transition.Child
+				as={Fragment}
+				enter="ease-out duration-300"
+				enterFrom="opacity-0"
+				enterTo="opacity-100"
+				leave="ease-in duration-200"
+				leaveFrom="opacity-100"
+				leaveTo="opacity-0"
+			>
+				<div className="fixed inset-0 bg-black bg-opacity-50" />
+			</Transition.Child>
+
+			<div className="fixed inset-0 overflow-y-auto">
+				<div className="flex min-h-full items-center justify-center p-4 text-center">
+				<Transition.Child
+					as={Fragment}
+					enter="ease-out duration-300"
+					enterFrom="opacity-0 scale-95"
+					enterTo="opacity-100 scale-100"
+					leave="ease-in duration-200"
+					leaveFrom="opacity-100 scale-100"
+					leaveTo="opacity-0 scale-95"
+				>
+					<Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-gradient-to-tr from-[#181424] via-buttontransparent2 to-buttontransparent border-t-[0.5px] border-[#474747] p-6 text-left align-middle shadow-xl transition-all">
+						<Dialog.Title
+							as="h3"
+							className="text-lg font-medium leading-6 text-white"
+						>
+						Create Your Account
+						</Dialog.Title>
+						<div className="flex flex-col mt-2 gap-y-2">
+							<p className="text-sm text-gray-500">
+							Your market account is your portal to the Orbit network, it lets you buy and sell items as well as earn revenue!
+							</p>
+							<div className="flex flex-col">
+								<label className="text-gray-500 font-bold">Nickname</label>
+								<input type="text" value={nickName} onChange={(e) => {setNickName(e.target.value)}}/>
+							</div>
+						</div>
+						<div className="mt-4">
+							<button
+							type="button"
+							className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+							onClick={closeModal}
+							>
+								Create Account!
+							</button>
+						</div>
+					</Dialog.Panel>
+				</Transition.Child>
+				</div>
+			</div>
+			</Dialog>
+		</Transition>
+    </>
+  )
+}
