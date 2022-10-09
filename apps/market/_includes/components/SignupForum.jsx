10162a2811f16fd01549cba6ef248d850270f74a
0xMillionz <madokakaname99@protonmail.com>
2022-10-09 11:33:27 -0400

    spacing changes

--- apps/market/_includes/components/SignupForum.jsx
+++ apps/market/_includes/components/SignupForum.jsx
@@ -11,7 +11,7 @@ export function SignupForum(props) {
 		<div className="rounded-xl max-w-xl bg-[#141619] py-10 px-20 mx-auto">
 			<h1 className="text-4xl text-white font-bold">Sign Up</h1>
 			<span className="text-[#848484]">
-			You account is the key to the Orbit marketplace network. Create an account to list and buy products!
+			You account is the key to the Orbit market network. Create an account to list and buy products!
 			</span>
 			<div className="group cursor-pointer relative my-5 mx-auto justify-center w-fit">
 				<div className="flex absolute bottom-5 right-5 bg-blue-500 rounded-full h-8 w-8 align-middle">
@@ -23,7 +23,7 @@ export function SignupForum(props) {
 				<div className="flex flex-col justify-start gap-y-2">
 					<label className="font-bold text-white text-xl">Username<span className="text-red-500">*</span></label>
 					<input
-						className="rounded-lg p-2 text-white text-xl border-2 border-[#40444F] bg-[#222429] placeholder:font-bold placeholder:text-[#454545]"
+						className="rounded-lg p-2 text-white text-lg border-2 border-[#40444F] bg-[#222429] placeholder:font-bold placeholder:text-[#454545]"
 						placeholder="Enter Username"
 						value={userName}
 						onChange={(e) => {setUserName(e.target.value)}}
@@ -32,7 +32,7 @@ export function SignupForum(props) {
 				<div className="flex flex-col justify-start gap-y-2">
 					<label className="font-bold text-white text-xl">Name<span className="text-red-500">*</span></label>
 					<input
-						className="rounded-lg p-2 text-white text-xl border-2 border-[#40444F] bg-[#222429] placeholder:font-bold placeholder:text-[#454545]"
+						className="rounded-lg p-2 text-white text-lg border-2 border-[#40444F] bg-[#222429] placeholder:font-bold placeholder:text-[#454545]"
 						placeholder="Enter Name"
 						value={name}
 						onChange={(e) => {setName(e.target.value)}}
@@ -41,7 +41,7 @@ export function SignupForum(props) {
 				<div className="flex flex-col justify-start gap-y-2">
 					<label className="font-bold text-white text-xl">Bio</label>
 					<textarea
-						className="rounded-lg p-2 text-white text-xl border-2 border-[#40444F] bg-[#222429] placeholder:font-bold placeholder:text-[#454545]"
+						className="rounded-lg p-2 text-white text-lg border-2 border-[#40444F] bg-[#222429] placeholder:font-bold placeholder:text-[#454545]"
 						placeholder="Enter Bio"
 						value={bio}
 						onChange={(e) => {setBio(e.target.value)}}
@@ -72,10 +72,10 @@ export function SignupModal(props) {
 	const [bio, setBio] = useState();
 
 	return(
-		<div className="rounded-xl max-w-lg bg-[#141619] py-10 px-20 mx-auto">
+		<div className="rounded-xl max-w-lg bg-[#141619] py-10 px-[4rem] mx-auto">
 			<h1 className="text-3xl text-white font-bold">Sign Up</h1>
 			<span className="text-[#848484]">
-			You account is the key to the Orbit marketplace network. Create an account to list and buy products!
+			You account is the key to the Orbit market network. Create an account to list and buy products!
 			</span>
 			<div className="group cursor-pointer relative my-5 mx-auto justify-center w-fit">
 				<div className="flex absolute bottom-5 right-5 bg-blue-500 rounded-full h-8 w-8 align-middle">
@@ -87,16 +87,16 @@ export function SignupModal(props) {
 				<div className="flex flex-col justify-start gap-y-1">
 					<label className="font-bold text-white text-lg">Username<span className="text-red-500">*</span></label>
 					<input
-						className="rounded-lg p-2 text-white text-lg border-2 border-[#40444F] bg-[#222429] placeholder:font-bold placeholder:text-[#454545]"
+						className="rounded-lg p-2 text-white border-2 border-[#40444F] bg-[#222429] placeholder:font-bold placeholder:text-[#454545]"
 						placeholder="Enter Username"
 						value={userName}
 						onChange={(e) => {setUserName(e.target.value)}}
 					/>
 				</div>
 				<div className="flex flex-col justify-start gap-y-1">
-					<label className="font-bold text-white text-xl">Name<span className="text-red-500">*</span></label>
+					<label className="font-bold text-white text-lg">Name<span className="text-red-500">*</span></label>
 					<input
-						className="rounded-lg p-2 text-white text-xl border-2 border-[#40444F] bg-[#222429] placeholder:font-bold placeholder:text-[#454545]"
+						className="rounded-lg p-2 text-white border-2 border-[#40444F] bg-[#222429] placeholder:font-bold placeholder:text-[#454545]"
 						placeholder="Enter Name"
 						value={name}
 						onChange={(e) => {setName(e.target.value)}}
@@ -105,13 +105,13 @@ export function SignupModal(props) {
 				<div className="flex flex-col justify-start gap-y-1">
 					<label className="font-bold text-white text-lg">Bio</label>
 					<textarea
-						className="rounded-lg p-2 text-white text-lg border-2 border-[#40444F] bg-[#222429] placeholder:font-bold placeholder:text-[#454545]"
+						className="rounded-lg p-2 text-white border-2 border-[#40444F] bg-[#222429] placeholder:font-bold placeholder:text-[#454545]"
 						placeholder="Enter Bio"
 						value={bio}
 						onChange={(e) => {setBio(e.target.value)}}
 					/>
 				</div>
-				<button className="flex flex-row gap-x-2 mt-6 mx-auto text-xl rounded-full font-bold text-white bg-gradient-to-tr from-[#181424] via-buttontransparent2 to-buttontransparent border-t-[0.5px] border-[#474747] bg-transparent align-middle my-auto px-4 py-2">
+				<button className="flex flex-row gap-x-2 mt-6 mx-auto text-lg rounded-full font-bold text-white bg-gradient-to-tr from-[#181424] via-buttontransparent2 to-buttontransparent border-t-[0.5px] border-[#474747] bg-transparent align-middle my-auto px-4 py-2">
 					<div className="relative flex flex-shrink-0 h-6 w-6 align-middle my-auto">
 						<Image
 							src="/orbit.png"
