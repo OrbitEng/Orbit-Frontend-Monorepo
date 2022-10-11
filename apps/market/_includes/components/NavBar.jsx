5630ac5508e6e4ad393ee7cccae88cfc92cba0cf
0xMillionz <madokakaname99@protonmail.com>
2022-10-10 23:34:09 -0400

    fixing z height overlap header

--- apps/market/_includes/components/NavBar.jsx
+++ apps/market/_includes/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Fragment } from "react"
 export function NavBar(props){
     return (
         <Transition
-        className="absolute top-0 justify-self-center align-middle justify-center mx-auto w-full"
+        className="absolute top-0 z-10 justify-self-center align-middle justify-center mx-auto w-full"
         appear={true}
         show={true}
         enter="transition-opacity duration-500"
@@ -14,7 +14,7 @@ export function NavBar(props){
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <nav className="relative h-32 hidden w-1/3 lg:flex flex-row align-middle gap-10 text-sm lg:text-lg mx-auto justify-evenly">
+        <nav className="relative h-32 hidden w-1/3 lg:flex flex-row align-middle gap-10 z-10 text-sm lg:text-lg mx-auto justify-evenly">
             <button className="relative bg-transparent font-bold text-[#BBBBBB] transition hover:scale-105">Home</button>
             <button className="relative bg-transparent font-bold text-[#606060] transition hover:scale-105">Explore</button>
             <button className="relative bg-transparent font-bold text-[#606060] transition hover:scale-105">Learn</button>
