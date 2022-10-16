0a19cc4b44e42e5307bf061f225f641dbcee0d98
Your Name <you@example.com>
2022-10-15 23:46:48 -0400

    idek

--- apps/market/_layouts/SellLayouts/PhysicalSellLayout.jsx
+++ apps/market/_layouts/SellLayouts/PhysicalSellLayout.jsx
@@ -36,7 +36,8 @@ export function PhysicalUploadForm(props) {
 	const [prodName, setProdName] = useState("");
     const [delivery, setDelivery] = useState();
     const [quantity, setQuantity] = useState();
-	const [description, setDescription] = useState();
+	const [description, setDescription] = useState("");
+    const [listRecent, setListRecent] = useState(false);
 	
 	const [files, setFiles] = useState([]);
     const [bigPreviewSrc, setBigPreviewSrc] = useState(null);
@@ -279,10 +280,14 @@ export function PhysicalUploadForm(props) {
                                     onChange={(e)=>{setDescription(e.target.value)}}
                                 />
                             </div>
+                            <div className="w-full flex flex-row text-white justify-center">
+                                <input type={"checkbox"} onChange={()=>{setListRecent(!listRecent)}}/>
+                                <span className="mx-8">have product displayed in "recent listings" on the front page</span>
+                            </div>
                             <div className="bg-[#171717] px-6 rounded-full flex justify-center mx-auto border-t-[0.5px] border-[#474747] hover:scale-105 transition duration-200 ease-in-out">
                                 <button className="text-transparent py-2 bg-clip-text font-bold bg-gradient-to-tr from-[#8BBAFF] to-[#D55CFF] mx-auto text-2xl rounded-full" onClick={()=>{
                                     ListProduct(
-                                        currency, price, delivery, prodName, description, quantity, files
+                                        currency, price, delivery, prodName, description, quantity, files, listRecent
                                     )
                                 }}>
                                     Upload
