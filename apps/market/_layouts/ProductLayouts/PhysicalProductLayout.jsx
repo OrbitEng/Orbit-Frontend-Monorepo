80c8bd8701ab050e9c4fa5ece8915f6234be6337
0xMillionz <madokakaname99@protonmail.com>
2022-10-24 21:37:42 -0400

    fixing more product display styles

--- apps/market/_layouts/ProductLayouts/PhysicalProductLayout.jsx
+++ apps/market/_layouts/ProductLayouts/PhysicalProductLayout.jsx
@@ -78,7 +78,7 @@ export function PhysicalProductLayout(props) {
 	},[userAccount])
 
 	return(
-		<div className="flex flex-row w-[90%] mx-auto mt-6 mb-20 h-[80vh] gap-8">
+		<div className="flex flex-row max-w-6xl mx-auto mt-6 mb-20 h-[80vh] gap-8">
 			<div className={"h-full w-1/2 p-10 flex flex-col gap-y-5 align-middle text-ellipsis " + (descriptionOpen ? " overflow-y-auto" : " overflow-hidden")}>
 				<div className="mt-auto flex w-56 items-center content-center rounded-full shadow-lg bg-gradient-to-tr from-[#23232375] via-[#0F0F0F4D] to-[#151515B3]">
 					<div className="flex content-start rounded-full mx-2 py-2 pr-4 gap-x-3">
@@ -110,14 +110,41 @@ export function PhysicalProductLayout(props) {
 					</div>
 					<div className="font-semibold align-top ml-3">
 						<span className="text-white text-xl align-top mr-2">Price:{' '}</span>
-						<span className="text-[#5a5a5a] text-3xl align-top">
+						<span className="text-[#5a5a5a] text-4xl align-top">
 							{
 								("$" + props.prodInfo?.data?.metadata?.price?.toString() || "Custom")
 							}
 						</span>
 					</div>
 				</div>
-				<div className="flex flex-col bg-[#484848] bg-opacity-10 rounded-xl py-5 px-4 w-full bottom-0 overflow-hidden">
+				<div className="flex flex-row w-full justify-center mt-4">
+					{
+						// FIXME(millionz): eventually more types will come along and break this
+						(
+							isOwner && 
+							<div className="flex flex-row justify-center">
+								<EditPhysicalProductModal selectedProduct={props.prodInfo}/>
+							</div> 
+						) || 
+						(
+							(buyerTxLog == undefined) &&
+							<BuyerTxLogModal category={"physical"} setTxLog={setBuyerTxLog}/>
+						) ||
+						(
+							<div className="flex flex-row gap-x-4">
+								<button
+									className="font-semibold p-3 text-white bg-gradient-to-t from-[#000] to-[#0F1025] rounded-full drop-shadow text-lg border-2 border-[#2C2C4A]"
+									onClick={()=>{AddItem(props.prodInfo)}}
+								>🛒 Add to Cart</button>
+								<button
+									className="font-semibold p-3 text-white bg-gradient-to-t from-[#000] to-[#0F1025] rounded-full drop-shadow text-lg border-2 border-[#2C2C4A]"
+									onClick={()=>{setOpenPos(true)}}
+								>⚡ Quick Buy</button>
+							</div>
+						)
+					}
+				</div>
+				<div className="flex flex-col bg-[#484848] bg-opacity-10 rounded-2xl py-5 px-4 w-full overflow-hidden mb-auto mt-4">
 					<button
 						className="text-2xl text-white font-bold flex flex-row border-b-[1px] border-[#636363] w-full mb-2 pb-1 focus:outline-none"
 						onClick={() => {setDescriptionOpen(!descriptionOpen)}}
@@ -134,28 +161,6 @@ export function PhysicalProductLayout(props) {
 							descriptionOpen ? props.prodInfo?.data?.metadata?.info?.description : props.prodInfo?.data?.metadata?.info?.description?.slice(0,218) + (props.prodInfo?.data?.metadata?.info?.description.length > 218 ? "..." : "")
 						}
 					</p>
-				</div>
-				<div className="flex flex-row w-full justify-center mt-6 mb-auto">
-					{
-						// FIXME(millionz): eventually more types will come along and break this
-						(
-							isOwner && 
-							<div className="flex flex-row justify-center">
-								<EditPhysicalProductModal selectedProduct={props.prodInfo}/>
-							</div> 
-						) || 
-						(
-							(buyerTxLog == undefined) &&
-							<BuyerTxLogModal category={"physical"} setTxLog={setBuyerTxLog}/>
-						) ||
-						(
-							<div className="flex flex-row gap-x-4">
-								<button className="font-semibold p-3 text-white bg-gradient-to-t from-[#000] to-[#0F1025] rounded-full drop-shadow text-lg border-2 border-[#2C2C4A]"
-								onClick={()=>{AddItem(props.prodInfo)}}>🛒 Add to Cart</button>
-								<button className="font-semibold p-3 text-white bg-gradient-to-t from-[#000] to-[#0F1025] rounded-full drop-shadow text-lg border-2 border-[#2C2C4A]" onClick={()=>{setOpenPos(true)}}>⚡ Quick Buy</button>
-							</div>
-						)
-					}
 				</div>
 				{itemAsCart && <PosModal openPos={openPos} setOpenPos={setOpenPos} cart={itemAsCart} setCart={setItemAsCart} />}
 			</div>
