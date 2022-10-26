cb9989f47bd007b435ca76506ce61e520cc1fbfa
0xMillionz <madokakaname99@protonmail.com>
2022-10-26 16:25:07 -0400

    working chat drawer with deleted content

--- apps/market/_includes/Chat.jsx
+++ apps/market/_includes/Chat.jsx
@@ -12,7 +12,7 @@ export function ChatWidget(props) {
 	const [chatSearch, setChatSearch] = useState();
 	const [chatMessage, setChatMessage] = useState();
 	const {chatState, setChatState} = useContext(ChatCtx);
-	const [panel, setPanel] = useState("convos"); // can either be "convos" or "text"
+	const [panel, setPanel] = useState("collapsed"); // can either be "convos" or "text"
 
 	function classNames(...classes) {
 		return(classes.filter(Boolean).join(' '))
@@ -21,209 +21,22 @@ export function ChatWidget(props) {
 	useEffect(() => {console.log(chatState)}, [chatState])
 
 	return (
-		<>
-			<div className="fixed flex flex-col inset-y-0 right-0 mt-80">
+		<div className="fixed flex flex-col inset-y-0 right-0 mt-[40rem]">
+			<div className={"pointer-events-auto transition-all duration-300 right-0 relative w-screen flex flex-row z-[130] h-96 " + (( chatState.isOpen ? (panel === "text" ? "max-w-3xl" : "max-w-md") : " max-w-[3.5rem]")) }>
 				<button 
-					className={"absolute flex flex-col right-0 h-14 w-14 bg-[#251F30] z-[130] rounded-l-lg focus:outline-none"}
-					onClick={() => {setChatState(s => ({ ...s, isOpen: true}))}}
+					className={"relative flex flex-col flex-shrink-0 w-14 h-full bg-[#251F30] z-[130] rounded-l-lg focus:outline-none"}
+					onClick={() => {setChatState(s => ({ ...s, isOpen: !chatState.isOpen}))}}
 				>
 					<div className="my-auto mx-auto">
 						<EnvelopeIcon className="h-6 w-6 stroke-2 text-white" />
 					</div>
 				</button>
-			<Transition
-				show={chatState.isOpen}
-				as={Fragment}
-				enter="transform transition ease-in-out duration-500"
-				enterFrom="translate-x-full"
-				enterTo="translate-x-0"
-				leave="transform transition ease-in-out duration-500"
-				leaveFrom="translate-x-0"
-				leaveTo="translate-x-full"
-			>
-				<div className={"pointer-events-auto relative w-screen flex z-[130] mt-14 " + (panel === "text" ? "max-w-3xl" : "max-w-md") }>
-					<div className="relative flex flex-col -inset-y-16 right-0 mb-auto mt-32">
-						<button 
-							className={"relative flex flex-col h-14 w-14  border-l-[1px] border-y-[1px] border-[#545454] border-opacity-30 z-[130] focus:outline-none rounded-l-lg " + (panel === "convos" ? "bg-[#303030]" : "bg-[#251F30]")}
-							onClick={() => setPanel("convos")}
-						>
-							<div className="my-auto mx-auto">
-								<UserGroupIcon className="h-6 w-6 text-white"/>
-							</div>
-						</button>
-						<button 
-							className={"relative flex flex-col h-14 w-14  border-l-[1px] border-y-[1px] border-[#545454] border-opacity-30 z-[130] focus:outline-none rounded-l-lg " + (panel === "text" ? "bg-[#303030]" : "bg-[#251F30]")}
-							onClick={() => setPanel("text")}
-						>
-							<div className="my-auto mx-auto">
-								<ChatBubbleLeftEllipsisIcon className="h-6 w-6 text-white"/>
-							</div>
-						</button>
-					</div>
-					<div className="flex flex-row backdrop-blur-xl h-[30rem] w-full overflow-hidden bg-gradient-to-t from-[#32254E78] to-[#26232C9C] border-t border-x border-[#545454] border-opacity-30 shadow-xl my-auto rounded-l-xl">
-						<div className="absolute top-0 left-0 flex pt-4 pl-4 justify-start">
-							<button
-								type="button"
-								className="rounded-full text-white bg-[#303030] bg-opacity-70 p-1 border-[#5b5b5b] border-[1px] focus:outline-none"
-								onClick={() => {setChatState(s => ({...s, isOpen: false}))}}
-							>
-								<span className="sr-only">Close Chat</span>
-								<XMarkIcon className="h-4 w-4 text-[#e2e2e2]" aria-hidden="true" />
-							</button>
-						</div>
-						<div className="flex flex-row h-full w-full">
-						{panel === "convos" ? (
-							<div className="flex flex-col w-full h-full p-3 flex-shrink-0 bg-gradient-to-t from-[#2917514D] to-[#1D045178]">
-								<div className="relative flex h-16 w-16 rounded-full overflow-hidden mx-auto mt-10">
-									<Image 
-										layout="fill"
-										src={userAccount?.data?.profilePic || "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?d=mp&f=y"}
-										objectFit="cover"
-									/>
-								</div>
-								<span className="font-bold text-2xl text-white mx-auto truncate">{userAccount?.data?.metadata?.name}</span>
-								<span className="font-semibold text-sm text-[#515661] truncate mx-14">{("@" + userAccount?.address.toString())}</span>
-								<div className="flex flex-row bg-white bg-opacity-5 p-2 rounded-lg gap-x-1 mt-4 mx-4">
-									<MagnifyingGlassIcon className="h-4 w-4 my-auto text-[#5F5F5F]"/>
-									<input
-										className="bg-transparent text-sm outline-none text-[#515661] placeholder:text-[#5F5F5F]"
-										type="text"
-										placeholder="Search"
-										value={chatSearch}
-										onChange={(e) => {setChatSearch(e.target.value)}}
-									/>
-								</div>
-								<div className="flex flex-col mt-6">
-									<span className="text-white text-xs font-bold p-2">Messages <span className="text-blue-500">{props?.notis && " (" + props.notis + ")"}</span></span>
-									<div className="flex flex-row rounded-lg w-full gap-x-3 p-3 bg-gradient-to-r from-[#4A165386] to-[#1F165386]">
-										<div className="relative flex h-8 w-8 rounded-full overflow-hidden">
-											<Image 
-												layout="fill"
-												src={props?.vendor?.profilePic || "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?d=mp&f=y"}
-												objectFit="cover"
-											/>
-										</div>
-										<div className="flex flex-col text-white font-bold text-xl align-middle my-auto justify-start">
-											<span className="text-sm text-[#] -mb-[3px]">{props?.vendor?.nickname || "UserName"}</span>
-											<span className="text-[#535353] text-xs font-normal">{(props?.sellerAddr?.slice(0,10) + "...")  || "DMgY6wi2FV..."}</span>
-										</div>
-										<div className="flex justify-end text-white text-xs flex-grow">{props?.timestamp || "hh:mm"}</div>
-									</div>
-									<div className="flex flex-row rounded-lg w-full gap-x-3 p-3 bg-transparent">
-										<div className="relative flex h-8 w-8 rounded-full overflow-hidden">
-											<Image 
-												layout="fill"
-												src={props?.vendor?.profilePic || "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?d=mp&f=y"}
-												objectFit="cover"
-											/>
-										</div>
-										<div className="flex flex-col text-white font-bold text-xl align-middle my-auto justify-start">
-											<span className="text-sm text-[#] -mb-[3px]">{props?.vendor?.nickname || "UserName"}</span>
-											<span className="text-[#535353] text-xs font-normal">{(props?.sellerAddr?.slice(0,10) + "...")  || "DMgY6wi2FV..."}</span>
-										</div>
-										<div className="flex justify-end text-white text-xs flex-grow">{props?.timestamp || "hh:mm"}</div>
-									</div>
-									<div className="flex flex-row rounded-lg w-full gap-x-3 p-3 bg-transparent">
-										<div className="relative flex h-8 w-8 rounded-full overflow-hidden">
-											<Image 
-												layout="fill"
-												src={props?.vendor?.profilePic || "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?d=mp&f=y"}
-												objectFit="cover"
-											/>
-										</div>
-										<div className="flex flex-col text-white font-bold text-xl align-middle my-auto justify-start">
-											<span className="text-sm text-[#] -mb-[3px]">{props?.vendor?.nickname || "UserName"}</span>
-											<span className="text-[#535353] text-xs font-normal">{(props?.sellerAddr?.slice(0,10) + "...")  || "DMgY6wi2FV..."}</span>
-										</div>
-										<div className="flex justify-end text-white text-xs flex-grow">{props?.timestamp || "hh:mm"}</div>
-									</div>
-								</div>
-							</div>
-							) : (
-							<div className="relative flex flex-col flex-grow bg-gradient-to-t from-[#2917514D] to-[#1D045178] overflow-hidden p-4 pt-14">
-								<div className="sticky w-full bg-[#2C2638] bg-opacity-30 rounded-lg">
-									<div className="flex flex-row my-auto rounded-lg w-full gap-x-3 p-3 bg-transparent">
-										<div className="relative flex h-8 w-8 rounded-full overflow-hidden">
-											<Image 
-												layout="fill"
-												src={props?.vendor?.profilePic || "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?d=mp&f=y"}
-												objectFit="cover"
-											/>
-										</div>
-										<div className="flex flex-col my-auto text-white font-bold text-xl align-middle justify-start">
-											<span className="text-sm -mb-[3px]">{props?.vendor?.nickname || "UserName"}</span>
-											<span className="text-[#535353] text-xs font-normal">{"@somemarketaddress"}</span>
-										</div>
-										<button 
-											className="bg-transparent ml-auto"
-											onClick={() => {}}
-										>
-											<InformationCircleIcon className="h-6 w-6 text-[#6D6D6D] stroke-2" />
-										</button>
-									</div>
-								</div>
-								<div className="px-5 overflow-y-scroll">
-									<div className="relative flex flex-col flex-grow">
-										{/* something something insert conditional for messages here */}
-										<Message text="hello"/>
-										<SelfMessage text="hello" />
-										<Message text="hello"/>
-										<SelfMessage text="hello" />
-										<Message text="hello"/>
-										<SelfMessage text="hello" />
-										<Message text="hello"/>
-										<SelfMessage text="hello" />
-										<Message text="hello"/>
-										<SelfMessage text="hello" />
-										<Message text="hello"/>
-										<SelfMessage text="hello" />
-										<Message text="hello"/>
-										<SelfMessage text="hello" />
-										<SelfMessage text="hello" />
-										<Message text="hello"/>
-										<Message text="hello"/>
-										<SelfMessage text="hello" />
-										<Message text="hello"/>
-										<SelfMessage text="hello" />
-										<Message text="hello"/>
-										<SelfMessage text="hello" />
-										<Message text="hello"/>
-										<Message text="hello"/>
-										<Message text="hello"/>
-										<Message text="hello"/>
-										<Message text="hello"/>
-										<Message text="hello"/>
-										<Message text="hello"/>
-										<Message text="hello"/>
-										<Message text="hello"/>
-										<Message text="hello"/>
-										<ContractRequest autoFocus requestName="Custom Logo" />
-									</div>
-								</div>
-								<div className="sticky flex flex-row mx-3 bottom-0 mb-4 inset-x-0 p-3 bg-white bg-opacity-5 rounded-lg">
-									<input
-										className="flex flex-grow bg-transparent text-sm outline-none text-[#949494] placeholder:text-[#949494]"
-										type="text"
-										placeholder="Write a message..."
-										value={chatMessage}
-										onChange={(e) => {setChatMessage(e.target.value)}}
-									/>
-									<div className="flex flex-row justify-between w-28">
-										<PaperClipIcon className="h-5 w-5 text-[#949494]" />
-										<CloudArrowUpIcon className="h-5 w-5 text-[#949494]"/>
-										<TagIcon className="h-5 w-5 text-[#949494]"/>
-										<PencilSquareIcon className="h-5 w-5 text-[#949494]" />
-									</div>
-								</div>
-							</div>
-							)}
-						</div>
-					</div>
+				<div className={"bg-white relative w-full"}>
+				Hello
 				</div>
-			</Transition>
 			</div>
-			
-		</>
+		</div>
+				
 	)
 }
 
