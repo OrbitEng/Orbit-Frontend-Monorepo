b4760bef706006c8b6ed6d7e08fd11046081dcf4
0xMillionz <madokakaname99@protonmail.com>
2022-10-26 21:55:54 -0400

    more sell layout changes

--- apps/market/_layouts/SellLayouts/BaseSellLayout.jsx
+++ apps/market/_layouts/SellLayouts/BaseSellLayout.jsx
@@ -45,10 +45,10 @@ export function SellLayout(props){
 							leaveTo="opacity-0 scale-95 "
 						>
 							<div
-								className="flex group relative rounded-2xl my-auto h-1/2 w-1/3 hover:scale-[103%] transition duration-700"
+								className="flex group relative rounded-3xl my-auto h-1/2 w-1/3 hover:scale-[103%] transition duration-700"
 							>
 								<div className="bg-[#26308F] absolute -inset-0 bg-opacity-70 rounded-lg blur-xl group-hover:bg-opacity-100 transition duration-700" />
-								<div className="flex flex-col py-4 px-8 relative bg-gradient-to-tr from-[#2c2c2cc0] to-[#4a4a4ac0] w-full h-full rounded-2xl">
+								<div className="flex flex-col py-4 px-8 relative bg-gradient-to-tr from-[#2c2c2cc0] to-[#4a4a4ac0] w-full h-full rounded-3xl">
 									<div className="relative flex w-1/2 h-1/2 mx-auto">
 										<Image
 											src="/emojis/globeEmojiImage.png"
@@ -57,10 +57,10 @@ export function SellLayout(props){
 										/>
 									</div>
 									<h1 className="text-3xl font-bold text-white mx-auto text-center">Physical</h1>
-									<p className="text-xl text-[#6A6A6A] mx-auto justify-center mt-4 leading-tight text-center">
-										Sell shoes, clothes tech, and much more with just a few clicks on the Orbit network!
+									<p className="text-xl text-[#6A6A6A] mx-auto justify-center mt-4 leading-tight text-center h-20">
+										Sell shoes, clothes, tech, and much more with just a few clicks!
 									</p>
-									<div className="rounded-full p-2 my-auto h-14 w-14 bg-gradient-to-tr from-[#0E0C15] to-[#18171D] via-[#161320] mx-auto content-center align-middle">
+									<div className="rounded-full p-2 my-auto h-14 w-14 bg-gradient-to-tr from-[#0E0C15] to-[#18171D] via-[#161320] mx-auto content-center align-middle shadow-md shadow-black">
 										<ArrowRightIcon className="h-10 w-10 text-[#3F46FF] m-auto stroke-2" />
 									</div>
 								</div>
@@ -80,10 +80,10 @@ export function SellLayout(props){
 								leaveTo="opacity-0 scale-95 "
 							>
 							<div
-								className="flex group relative rounded-2xl my-auto h-1/2 w-1/3 hover:scale-[103%] transition duration-700"
+								className="flex group relative rounded-3xl my-auto h-1/2 w-1/3 hover:scale-[103%] transition duration-700"
 							>
 								<div className="bg-[#4E268F] absolute -inset-0 bg-opacity-70 rounded-lg blur-xl group-hover:bg-opacity-100 transition duration-700" />
-								<div className="flex flex-col py-4 px-8 relative bg-gradient-to-tr from-[#2c2c2cc0] to-[#4a4a4ac0] w-full h-full rounded-2xl">
+								<div className="flex flex-col py-4 px-8 relative bg-gradient-to-tr from-[#2c2c2cc0] to-[#4a4a4ac0] w-full h-full rounded-3xl">
 									<div className="relative flex w-1/2 h-1/2 mx-auto">
 										<Image
 											src="/emojis/filesEmojiImage.png"
@@ -92,10 +92,10 @@ export function SellLayout(props){
 										/>
 									</div>
 									<h1 className="text-3xl font-bold text-white mx-auto text-center">Commissions</h1>
-									<p className="text-xl text-[#6A6A6A] mx-auto justify-center mt-4 leading-tight text-center">
-										Freelance and take comissions through Orbit, your buisness has never been easier!
+									<p className="text-xl text-[#6A6A6A] mx-auto justify-center mt-4 leading-tight text-center h-20">
+										Freelance and take commissions, your buisness has never been easier!
 									</p>
-									<div className="rounded-full p-2 my-auto h-14 w-14 bg-gradient-to-tr from-[#0E0C15] to-[#18171D] via-[#161320] mx-auto content-center align-middle">
+									<div className="rounded-full p-2 my-auto h-14 w-14 bg-gradient-to-tr from-[#0E0C15] to-[#18171D] via-[#161320] mx-auto content-center align-middle shadow-md shadow-black">
 										<ArrowRightIcon className="h-10 w-10 text-[#4E268F] m-auto stroke-2" />
 									</div>
 								</div>
@@ -115,10 +115,10 @@ export function SellLayout(props){
 							leaveTo="opacity-0 scale-95 "
 						>
 							<div
-								className="flex group relative rounded-2xl my-auto h-1/2 w-1/3 hover:scale-[103%] transition duration-700"
+								className="flex group relative rounded-3xl my-auto h-1/2 w-1/3 hover:scale-[103%] transition duration-700"
 							>
 								<div className="bg-[#81268F] absolute -inset-0 bg-opacity-70 rounded-lg blur-xl group-hover:bg-opacity-100 duration-700 transition" />
-								<div className="flex flex-col py-4 px-8 relative bg-gradient-to-tr from-[#2c2c2cc0] to-[#4a4a4ac0] w-full h-full rounded-2xl">
+								<div className="flex flex-col py-4 px-8 relative bg-gradient-to-tr from-[#2c2c2cc0] to-[#4a4a4ac0] w-full h-full rounded-3xl">
 									<div className="relative flex w-1/2 h-1/2 mx-auto">
 										<Image
 											src="/emojis/wrenchEmojiImage.png"
@@ -127,10 +127,10 @@ export function SellLayout(props){
 										/>
 									</div>
 									<h1 className="text-3xl font-bold text-white mx-auto text-center">Digital</h1>
-									<p className="text-xl text-[#6A6A6A] mx-auto justify-center mt-4 leading-tight text-center">
-										Get paid for your digital art, design packages, beats, private content and more!
+									<p className="text-xl text-[#6A6A6A] mx-auto justify-center mt-4 leading-tight text-center h-20">
+										Get paid for your digital art, designs, beats, private content and more!
 									</p>
-									<div className="rounded-full p-2 my-auto h-14 w-14 bg-gradient-to-tr from-[#0E0C15] to-[#18171D] via-[#161320] mx-auto content-center align-middle">
+									<div className="rounded-full p-2 my-auto h-14 w-14 bg-gradient-to-tr from-[#0E0C15] to-[#18171D] via-[#161320] mx-auto content-center align-middle shadow-md shadow-black">
 										<ArrowRightIcon className="h-10 w-10 text-[#FB3FFF] m-auto stroke-2" />
 									</div>
 								</div>
