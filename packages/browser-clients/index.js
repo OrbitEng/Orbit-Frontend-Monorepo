d155f7b1705e47c2c63578e8003cbb8d47909c8a
0xDEADBEAT <jasperbriggs677@gmail.com>
2022-11-03 12:16:05 -0400

    js.js

--- packages/browser-clients/index.js
+++ packages/browser-clients/index.js
@@ -1,4 +1,4 @@
-import {IDBClient, file_client, file_common, enc_common} from "./src/mod.js.js";
+import {IDBClient, file_client, file_common, enc_common} from "./src/mod.js";
 
 export {
     IDBClient,
