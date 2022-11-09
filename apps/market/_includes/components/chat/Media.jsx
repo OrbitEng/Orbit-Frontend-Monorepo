d9ce80a45c25ba66bf9fef7322237862283f3cf5
0xMillionz <madokakaname99@protonmail.com>
2022-11-08 23:37:52 -0500

    media tab funcs (idk if broken just a template rn)

--- apps/market/_includes/components/chat/Media.jsx
+++ apps/market/_includes/components/chat/Media.jsx
@@ -1,4 +1,5 @@
 import { PaperClipIcon, PhotoIcon } from '@heroicons/react/24/outline';
+import { fill } from 'next-pwa/cache';
 import Image from 'next/image';
 
 // Two props
@@ -6,7 +7,7 @@ import Image from 'next/image';
 // props.links -> [] all urls that don't end in file descriptors here
 export default function MediaDisplay(props) {
 	return(
-		<div className="flex flex-col gap-y-4 p-4 w-full">
+		<div className="flex flex-col gap-y-8 p-4 w-full">
 			<div className="flex flex-col basis-1/2">
 				<div className="flex flex-row justify-between">
 					<span className="text-2xl text-[#D7D7D7] font-bold">Media</span>
@@ -14,7 +15,17 @@ export default function MediaDisplay(props) {
 				{
 					props?.mediaUrls ? 
 					<div className="grid lg:grid-cols-2 lg:grid-rows-2">
-						
+						{
+							// got no idea if these work correctly plz populate :D
+							props.mediaUrls.map((mediaUrl) => {
+							<div className="relative col-span-1 row-span-1">
+								<Image
+									src={mediaUrl}
+									layout="fill"
+									objectFit="cover"
+								/>
+							</div>
+						})}
 					</div>
 					:
 					<div className="flex flex-col m-auto">
@@ -28,8 +39,10 @@ export default function MediaDisplay(props) {
 					<span className="text-2xl text-[#D7D7D7] font-bold">Links & Files</span>
 				</div>
 				{props?.links ? 
-					<div className="grid lg:grid-cols-2 lg:grid-rows-2">
-						
+					<div className="flex flex-col overflow-y-scroll">
+						{props.links.map((link, linkIndex) => {
+							<LinkDisplay linkImage={link.linkImage} name={link.linkName} url={link.url} date={link.date} />
+						})}
 					</div>
 					: 
 					<div className="flex flex-col m-auto">
@@ -38,6 +51,29 @@ export default function MediaDisplay(props) {
 					</div>
 				}
 			</div>
+		</div>
+	)
+}
+
+function LinkDisplay(props) {
+	return(
+		<div className="flex flex-row justify-between rounded-lg bg-[#231E2C] bg-opacity-50 w-full my-auto gap-x-4">
+			{props?.linkImage ?
+				<div className="relative h-7 w-7">
+					<Image
+						src={props?.linkImage}
+						layout="fill"
+						objectFit="contain"
+					/>
+				</div>
+				:
+				<PaperClipIcon className='h-7 w-7 text-white'/>
+			}
+			<div className="flex flex-col justify-self-start">
+				<span className="text-white text-lg font-bold">{props.name}</span>
+				<span className="text-[#4F4F4F] text-sm">{props.url}</span>
+			</div>
+			<span className="text-[#4F4F4F]">{props.date}</span>
 		</div>
 	)
 }
