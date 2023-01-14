8aa8d089450e07f6031c0a7c31cec64808f9599f
0xDEADBEAT <jasperbriggs677@gmail.com>
2023-01-14 03:27:12 -0500

    OK

--- apps/market/_layouts/AdminLayout.jsx
+++ apps/market/_layouts/AdminLayout.jsx
@@ -2,11 +2,37 @@ import { useContext, useState } from 'react';
 
 import { HomeHeader } from '@includes/MarketHeader';
 import { PRODUCT_PROGRAM, ACCOUNTS_PROGRAM } from 'orbit-clients';
+import { useWallet, useConnection } from '@solana/wallet-adapter-react';
+import { sendAndConfirmTransaction, Transaction } from '@solana/web3.js';
+import { useEffect, useCallback } from 'react';
+import { Connection } from '@solana/web3.js';
 
 export default function AdminLayout(){
 
+	const wallet = useWallet();
+	const { connection } = useConnection();
+
+	const [instructions, setInstructions] = useState({
+		voterInitIx: undefined,
+		listingsInitIx: undefined
+	})
+
+	// https://discord.com/channels/889577356681945098/889577399308656662/1022990506323615814
+	const ConfirmAdminTransaction = useCallback(async ()=>{
+		let tx = new Transaction();
+		if(instructions.voterInitIx){
+			tx.add(instructions.voterInitIx)
+		}
+		if(instructions.listingsInitIx){
+			tx.add(instructions.listingsInitIx)
+		}
+		await sendAndConfirmTransaction(
+			connection, tx, wallet
+		)
+
+	}, [connection, wallet, instructions])
+
 	return(
-        
 		<div className="bg-[#070513] w-full min-h-screen flex flex-col m-auto text-white">
             <HomeHeader/>
 			<div className="m-10 flex flex-col">
@@ -26,7 +52,11 @@ export default function AdminLayout(){
 					button
 				</button>
 			</div>
-			
+			<button className="bg-white m-2 border-2 w-32 h-12 text-black" onClick={async (e) => {
+				await ConfirmAdminTransaction()
+			}}>
+					confirm
+			</button>
 		</div>
 	)
 }
